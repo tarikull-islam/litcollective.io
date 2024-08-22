d4efb6fcc116a030e02b0df3ba8015d9e49544e1
MD. TARIKUL ISLAM <sifatislam1316@gmail.com>
2024-08-22 21:12:27 +0600

    language select js config..

--- js/main.js
+++ js/main.js
@@ -13,5 +13,41 @@ function flyMenuToggle() {
         flyMenuHandle();
       }
     });
+    // ! ------------------------
+    function langHandle() {
+      if (localStorage.getItem("lag")) {
+        console.log("None");
+        $(".select-btn .selected-text")[0].innerText =
+          localStorage.getItem("lag");
+        $(".select-btn .selected-flag").attr(
+          "src",
+          localStorage.getItem("flag")
+        );
+      }
+    }
+    // ! ------------------
+    langHandle();
+    function linkHandle() {
+      document.body.classList.remove("fly-menu");
+    }
+    $(".nav-link").click(function (e) {
+      linkHandle();
+    });
+    // ! --------------
+    function languageSelect() {
+      $(".option").click(function (e) {
+        $(".select-btn .selected-text")[0].innerText = e.target.innerText;
+        $(".select-btn .selected-flag").attr(
+          "src",
+          e.currentTarget.children[0].getAttribute("src")
+        );
+        localStorage.setItem("lag", e.target.innerText);
+        localStorage.setItem(
+          "flag",
+          e.currentTarget.children[0].getAttribute("src")
+        );
+      });
+    }
+    languageSelect();
   });
 })(jQuery);
